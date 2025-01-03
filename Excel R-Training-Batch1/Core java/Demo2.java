7eebe5dd4be9b7274f54100ff2ad0dd329a92cb6
sohansaiteja <sohansaiteja@gmail.com>
2025-01-03 14:47:11 +0530

    Add files via upload
    second java code

---
+++ Excel R-Training-Batch1/Core java/Demo2.java
@@ -0,0 +1,15 @@
+public class Demo2 {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 20;
+
+        if (num1 > num2) {
+            System.out.println("greatest number:" + num1);
+
+        } else {
+            System.out.println("greatest number:" + num2);
+        }
+
+    }
+
+}
