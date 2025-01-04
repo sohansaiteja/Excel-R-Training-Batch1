3775f0e704672658f1fc6dfffd62d44c6c42cd7b
sohansaiteja <sohansaiteja@gmail.com>
2025-01-04 17:13:23 +0530

    Add files via upload
    Demo5

---
+++ Excel R-Training-Batch1/Core java/Demo5.java
@@ -0,0 +1,12 @@
+public class Demo5 {
+    public static void main(String args[]) {
+        String username = "user1";
+        String password = "user_1";
+        while ((username == "user1") && (password == "user_1")) {
+            System.out.println("hello user1 happy new year");
+            break;
+        }
+
+    }
+
+}
