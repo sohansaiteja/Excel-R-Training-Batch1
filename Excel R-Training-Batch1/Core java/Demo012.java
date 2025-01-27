512745df324c8afd21e936eeadf5d6873cf3ce21
sohansaiteja <sohansaiteja@gmail.com>
2025-01-27 21:12:52 +0530

    Area of Circle

---
+++ Excel R-Training-Batch1/Core java/Demo012.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Demo012 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the radius of the circle: ");
+        double r = sc.nextDouble();
+
+        double area = (r * r * 3.14);
+
+        System.out.println("Area of Circle: " + area);
+
+    }
+}
