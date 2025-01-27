ca702272a8c5a5892b2cc6191b2b20b83fcbaea8
sohansaiteja <sohansaiteja@gmail.com>
2025-01-27 21:34:30 +0530

    Even or odd number

---
+++ Excel R-Training-Batch1/Core java/Demo034.java
@@ -0,0 +1,13 @@
+public class Demo034 {
+    public static void main(String[] args) {
+        int x = 9;
+        if (x % 2 == 0) {
+            System.out.println("x is even");
+        }
+        if (x == 0) {
+            System.out.println("x is zero");
+        } else {
+            System.out.println("x is odd");
+        }
+    }
+}
