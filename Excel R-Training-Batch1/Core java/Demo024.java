ab25ed25e8c5a34152cdb90ac0d6dadf7c4b27ec
sohansaiteja <sohansaiteja@gmail.com>
2025-01-27 21:24:18 +0530

    Friend's Details

---
+++ Excel R-Training-Batch1/Core java/Demo024.java
@@ -0,0 +1,12 @@
+public class Demo024 {
+    public static void main(String[] args) {
+        String[] f1 = { "Raju ", "Rohit ", "Rahul ", "Roshan ", "Rosov " };
+        String[] address = { "Hyd ", "KPHB ", "KPHB ", "Medak ", "Medak " };
+        long[] number = { 70324443, 24890112, 987839991, 776891011, 1090775432 };
+
+        for (int i = 0; i < f1.length; i++) {
+            System.out.println("Friend " + (i + 1) + " :" + f1[i] + address[i] + number[i]);
+        }
+
+    }
+}
