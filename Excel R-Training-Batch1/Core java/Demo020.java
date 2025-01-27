d14d5674321a99fbb2e60fc02a3e71716ecc0a80
sohansaiteja <sohansaiteja@gmail.com>
2025-01-27 21:20:36 +0530

    Fibonnaci Series

---
+++ Excel R-Training-Batch1/Core java/Demo020.java
@@ -0,0 +1,25 @@
+class Fibonnaci {
+    void fib(int n) {
+        System.out.println("Fibonnaci Series for " + n + " terms: ");
+        if (n <= 0) {
+            return;
+        }
+        int first = 0;
+        int second = 1;
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(first + " ");
+
+            int next = first + second;
+            first = second;
+            second = next;
+        }
+    }
+}
+
+public class Demo020 {
+    public static void main(String[] args) {
+        Fibonnaci f = new Fibonnaci();
+        f.fib(8);
+    }
+}
