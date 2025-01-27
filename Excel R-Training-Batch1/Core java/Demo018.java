2b3b6289fa2c09ba2693eabff0495ae6b592cf19
sohansaiteja <sohansaiteja@gmail.com>
2025-01-27 21:18:36 +0530

    Minutes to Years

---
+++ Excel R-Training-Batch1/Core java/Demo018.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class MinToYear {
+    void years(int min) {
+
+        int y = min / (365 * 24 * 60);
+        System.out.println("Years: " + y);
+    }
+}
+
+public class Demo018 {
+    public static void main(String[] args) {
+        MinToYear m = new MinToYear();
+
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Enter minutes: ");
+            int min = sc.nextInt();
+            m.years(min);
+        }
+    }
+}
