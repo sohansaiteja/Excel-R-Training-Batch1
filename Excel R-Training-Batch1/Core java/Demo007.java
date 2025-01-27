10d28b2d66fd1934ffdf69ffe0d0f234aeea21a6
sohansaiteja <sohansaiteja@gmail.com>
2025-01-27 21:11:33 +0530

    Checking username and password2

---
+++ Excel R-Training-Batch1/Core java/Demo007.java
@@ -0,0 +1,13 @@
+class Demo007 {
+    @SuppressWarnings("StringEquality")
+    public static void main(String[] args) {
+
+        String Username = "Sohan";
+        String Password = "Sohan446";
+
+        while ((Username == "Sohan") && (Password == "Sohan446")) {
+            System.out.println("Welcome to Mallareddy university");
+            break;
+        }
+    }
+}
